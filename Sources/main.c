16ec36d46d208d26555a2f13ccb1935f962b9359
Markus Mayer <development@widemeadows.de>
2013-10-28 00:45:01 +0100

    added LED demo

--- Sources/main.c
+++ Sources/main.c
@@ -3,24 +3,100 @@
  *
  */
 
-
-
-
-
 #include "derivative.h" /* include peripheral declarations */
+#include "bme.h"
 
+void stupid_delay();
 
+void setup_gpios_for_led()
+{
+	// Set system clock gating to enable gate to port B
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTD_MASK;
+	
+	// Set Port B, pin 18 and 19 to GPIO mode
+	PORTB_PCR18 = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;
+	PORTB_PCR19 = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;;
+	
+	// Set Port d, pin 1 GPIO mode
+	PORTD_PCR1 = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;;
+		
+	// Data direction for port B, pin 18 and 19 and port D, pin 1 set to output 
+	GPIOB_PDDR |= GPIO_PDDR_PDD(1<<18)
+				| GPIO_PDDR_PDD(1<<19);
+	GPIOD_PDDR |= GPIO_PDDR_PDD(1<<1);
+	
+	// LEDs are low active
+	GPIOB_PCOR  = 1<<18; // clear output to light red LED
+	GPIOB_PCOR  = 1<<19; // clear output to light green LED
+	GPIOD_PSOR  = 1<<1;  // set output to clear blue LED
+}
 
 int main(void)
 {
-	int counter = 0;
+	setup_gpios_for_led();
 	
+	/*
+	SIM_SOPT2 |= SIM_SOPT2_UART0SRC(0b10); 
 	
+	// configure the UART0
+	UART0_BDH = 0b00000000;	// polling, polling, 1 stop bit, default baud
+	UART0_BDL = 0b00000100; // default baud
 	
+	UART0_C1 = 0b00000000; // all defaults
+	UART0_C2 = 0b00001100; // all defaults, but TX and RX enabled
 	
-	for(;;) {	   
-	   	counter++;
+	// wait for TX register to become empty
+	for (;;) {
+		int tx_register_full = (UART0_S1 & 0b10000000) == 0;
+		if (!tx_register_full) break;
+	}
+	
+	// send data
+	UART0_D = 0xA;
+	
+	// wait for TX register to become empty
+	for (;;) {
+		int tx_register_full = (UART0_S1 & 0b10000000) == 0;
+		if (!tx_register_full) break;
+	}
+	*/
+	
+	// LEDs are low active
+	GPIOB_PCOR  = 1<<18; // clear output to light red LED
+	GPIOB_PSOR  = 1<<19; // set output to clear green LED
+	GPIOD_PSOR  = 1<<1; // set output to clear blue LED
+
+	// Toggle the LEDs
+	for(;;) 
+	{	      	
+	   	GPIOB_PSOR = 1<<18;
+	   	GPIOB_PCOR = 1<<19;
+	   	GPIOD_PSOR = 1<<1;
+	   	stupid_delay();
+	   	
+	   	GPIOB_PSOR = 1<<18;
+		GPIOB_PSOR = 1<<19;
+		GPIOD_PCOR = 1<<1;
+		stupid_delay();
+		
+		GPIOB_PCOR = 1<<18;
+		GPIOB_PSOR = 1<<19;
+		GPIOD_PSOR = 1<<1;
+		stupid_delay();
 	}
 	
 	return 0;
+}
+
+
+void stupid_delay()
+{
+	uint16_t i, j;
+	for (i=0; i<65534U; ++i) 
+	{
+		for (j=0; j<160; ++j)
+		{
+			asm("nop");
+		}
+	}
 }
