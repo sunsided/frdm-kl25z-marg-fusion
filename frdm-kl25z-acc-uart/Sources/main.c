e7ceddc48a872f2679e0ed00976aec6a89ebbd31
Markus Mayer <development@widemeadows.de>
2013-10-29 22:37:36 +0100

    using defines for base pointers

--- frdm-kl25z-acc-uart/Sources/main.c
+++ frdm-kl25z-acc-uart/Sources/main.c
@@ -10,27 +10,35 @@
 #include "cpu/systick.h"
 #include "cpu/delay.h"
 
+#define SIM 	SIM_BASE_PTR
+#define PORTA	PORTA_BASE_PTR
+#define PORTB	PORTB_BASE_PTR
+#define PORTD	PORTD_BASE_PTR
+#define GPIOB	PTB_BASE_PTR
+#define GPIOD	PTD_BASE_PTR
+
+#define UART0	UART0_BASE_PTR
+
 void setup_gpios_for_led()
 {
 	// Set system clock gating to enable gate to port B
-	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTD_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTD_MASK;
 	
 	// Set Port B, pin 18 and 19 to GPIO mode
-	PORTB_PCR18 = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;
-	PORTB_PCR19 = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;
+	PORTB->PCR[18] = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;
+	PORTB->PCR[19] = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;
 	
 	// Set Port d, pin 1 GPIO mode
-	PORTD_PCR1 = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;;
+	PORTD->PCR[1] = PORT_PCR_MUX(1) | PORT_PCR_DSE_MASK;;
 		
 	// Data direction for port B, pin 18 and 19 and port D, pin 1 set to output 
-	GPIOB_PDDR |= GPIO_PDDR_PDD(1<<18)
-				| GPIO_PDDR_PDD(1<<19);
-	GPIOD_PDDR |= GPIO_PDDR_PDD(1<<1);
+	GPIOB->PDDR |= GPIO_PDDR_PDD(1<<18) | GPIO_PDDR_PDD(1<<19);
+	GPIOD->PDDR |= GPIO_PDDR_PDD(1<<1);
 	
 	// LEDs are low active
-	GPIOB_PCOR  = 1<<18; // clear output to light red LED
-	GPIOB_PCOR  = 1<<19; // clear output to light green LED
-	GPIOD_PSOR  = 1<<1;  // set output to clear blue LED
+	GPIOB->PCOR  = 1<<18; // clear output to light red LED
+	GPIOB->PCOR  = 1<<19; // clear output to light green LED
+	GPIOD->PSOR  = 1<<1;  // set output to clear blue LED
 }
 
 int main(void)
@@ -40,23 +48,22 @@ int main(void)
 	
 	setup_gpios_for_led();
 	
-	SIM_SOPT2 |= SIM_SOPT2_UART0SRC(0b10);	// set UART0 clock to oscillator
-	SIM_SCGC4 |= SIM_SCGC4_UART0_MASK;		// enable clock to UART0 module
+	SIM->SOPT2 |= SIM_SOPT2_UART0SRC(0b10);	// set UART0 clock to oscillator
+	SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;		// enable clock to UART0 module
 	
-	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK; 		// enable clock to port A (PTA1=rx, PTA2=tx)
-	PORTA_PCR1 = PORT_PCR_MUX(2) | PORT_PCR_DSE_MASK;	// alternative 2: RX
-	PORTA_PCR2 = PORT_PCR_MUX(2) | PORT_PCR_DSE_MASK;	// alternative 2: TX
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK; 		// enable clock to port A (PTA1=rx, PTA2=tx)
+	PORTA->PCR[1] = PORT_PCR_MUX(2) | PORT_PCR_DSE_MASK;	// alternative 2: RX
+	PORTA->PCR[2] = PORT_PCR_MUX(2) | PORT_PCR_DSE_MASK;	// alternative 2: TX
 		
 	// configure the UART0
-	UART0_BDH = 0b00000000 | UART_BDH_SBR(0); // polling, polling, 1 stop bit, default baud
-	UART0_BDL = UART_BDL_SBR(0b00000100) ; // default baud
+	UART0->BDH = 0b00000000 | UART_BDH_SBR(0); // polling, polling, 1 stop bit, default baud
+	UART0->BDL = UART_BDL_SBR(0b00000100) ; // default baud
 	
-	UART0_C1 = 0b00000000; // all defaults
-	UART0_C4 |= UART0_C4_OSR(15); // oversampling ratio of 16
+	UART0->C1 = 0b00000000; // all defaults
+	UART0->C4 |= UART0_C4_OSR(15); // oversampling ratio of 16
 	
 	// with SBR (modulo divisor): CLK / ((OSR+1)*SBR) = 125000
-	
-	UART0_C2 = 0b00001100; // all defaults, but TX and RX enabled
+	UART0->C2 = 0b00001100; // all defaults, but TX and RX enabled
 	
 	/*
 	// wait for TX register to become empty
@@ -76,41 +83,41 @@ int main(void)
 	*/
 	
 	// LEDs are low active
-	GPIOB_PCOR  = 1<<18; // clear output to light red LED
-	GPIOB_PSOR  = 1<<19; // set output to clear green LED
-	GPIOD_PSOR  = 1<<1; // set output to clear blue LED
+	GPIOB->PCOR  = 1<<18; // clear output to light red LED
+	GPIOB->PSOR  = 1<<19; // set output to clear green LED
+	GPIOD->PSOR  = 1<<1; // set output to clear blue LED
 
 	// Toggle the LEDs
 	for(;;) 
 	{
 		// red
-		GPIOB_PCOR = 1<<18;
-		GPIOB_PSOR = 1<<19;
-		GPIOD_PSOR = 1<<1;
+		GPIOB->PCOR = 1<<18;
+		GPIOB->PSOR = 1<<19;
+		GPIOD->PSOR = 1<<1;
 		delay_ms(1500);
 		
 		// yellow
-		GPIOB_PCOR = 1<<18;
-		GPIOB_PCOR = 1<<19;
-		GPIOD_PSOR = 1<<1;
+		GPIOB->PCOR = 1<<18;
+		GPIOB->PCOR = 1<<19;
+		GPIOD->PSOR = 1<<1;
 		delay_ms(1500);
 		
 		// green
-		GPIOB_PSOR = 1<<18;
-		GPIOB_PCOR = 1<<19;
-		GPIOD_PSOR = 1<<1;
+		GPIOB->PSOR = 1<<18;
+		GPIOB->PCOR = 1<<19;
+		GPIOD->PSOR = 1<<1;
 		delay_ms(1500);
 		
 		// all
-		GPIOB_PSOR = 1<<18;
-		GPIOB_PSOR = 1<<19;
-		GPIOD_PSOR = 1<<1;
+		GPIOB->PSOR = 1<<18;
+		GPIOB->PSOR = 1<<19;
+		GPIOD->PSOR = 1<<1;
 		delay_ms(10000);
 		
 		// blue
-		GPIOB_PSOR = 1<<18;
-		GPIOB_PSOR = 1<<19;
-		GPIOD_PCOR = 1<<1;
+		GPIOB->PSOR = 1<<18;
+		GPIOB->PSOR = 1<<19;
+		GPIOD->PCOR = 1<<1;
 		delay_ms(2000);
 	}
 	
