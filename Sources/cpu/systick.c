4cae7300ce1ba66b2109d51159c98bc15e78752b
Markus Mayer <development@widemeadows.de>
2013-10-29 22:11:44 +0100

    corrected the timer delay according to manual

--- Sources/cpu/systick.c
+++ Sources/cpu/systick.c
@@ -6,6 +6,7 @@
  */
 
 #include "derivative.h"
+
 #include "cpu/clock.h"
 #include "cpu/systick.h"
 
@@ -19,10 +20,10 @@
 void InitSysTick()
 {
 	/* see Cortex-M0+ Devices Generic User Guide, Section 4.4 */
-	SYST_RVR = CORE_CLOCK/SYSTICK_FREQUENCY;	/* set the reload value */
-	SYST_CSR = 	SysTick_CSR_ENABLE_MASK			/* enable the systick timer */ 
-				| SysTick_CSR_TICKINT_MASK 		/* enable interrupt if timer reaches zero */
-				| SysTick_CSR_CLKSOURCE_MASK;	/* use processor clock instead of external clock */
+	SysTick_BASE_PTR->RVR = CORE_CLOCK/SYSTICK_FREQUENCY - 1;	/* set the reload value */
+	SysTick_BASE_PTR->CSR = SysTick_CSR_ENABLE_MASK				/* enable the systick timer */ 
+							| SysTick_CSR_TICKINT_MASK 			/* enable interrupt if timer reaches zero */
+							| SysTick_CSR_CLKSOURCE_MASK;		/* use processor clock instead of external clock */
 }
 
 /**
