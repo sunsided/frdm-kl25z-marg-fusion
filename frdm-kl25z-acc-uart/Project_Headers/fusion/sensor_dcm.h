377e4354271e57ed0579226600e78d7bffa322f7
Markus Mayer <development@widemeadows.de>
2014-01-19 02:15:05 +0100

    adding more DCM functions

--- frdm-kl25z-acc-uart/Project_Headers/fusion/sensor_dcm.h
+++ frdm-kl25z-acc-uart/Project_Headers/fusion/sensor_dcm.h
@@ -10,19 +10,23 @@
 
 #include "compiler.h"
 #include "fixmatrix.h"
+#include "fixvector3d.h"
 
 /*!
 * \brief Builds a DCM from the given calibrated sensor values
-* \param[out] ddcm The DCM matrix to write to
-* \param[in] ax The x-axis accelerometer value.
-* \param[in] ay The y-axis accelerometer value.
-* \param[in] az The z-axis accelerometer value.
-* \param[in] gx The x-axis gyroscope value.
-* \param[in] gy The y-axis gyroscope value.
-* \param[in] gz The z-axis gyroscope value.
+* \param[out] dcm The DCM matrix to write to
+* \param[in] a The accelerometer vector.
+* \param[in] m The magnetometer vector
 */
-void sensor_dcm(mf16 *const ddcm, 
-    register const fix16_t *const ax, register const fix16_t *const ay, register const fix16_t *const az,
-    register const fix16_t *const mx, register const fix16_t *const my, register const fix16_t *const mz) HOT NONNULL;
+void sensor_dcm(mf16 *const dcm, 
+    register const v3d *RESTRICT const a, register const v3d *RESTRICT const m) HOT NONNULL;
+
+/*!
+* \brief Gets roll, pitch, yaw from DCM.
+* \param[out] roll The roll angle in radians.
+* \param[out] pitch The pitch(elevation) angle in radians.
+* \param[out] yaw The yaw(heading, azimuth) angle in radians.
+*/
+void sensor_dcm2rpy(mf16 *const dcm, fix16_t *RESTRICT const roll, fix16_t *RESTRICT const pitch, fix16_t *RESTRICT const yaw) HOT NONNULL;
 
 #endif // SENSOR_DDCM_H_
--- frdm-kl25z-acc-uart/Sources/fusion/sensor_dcm.c
+++ frdm-kl25z-acc-uart/Sources/fusion/sensor_dcm.c
@@ -1,21 +1,68 @@
 #include "fixmath.h"
+#include "fixarray.h"
 #include "fusion/sensor_dcm.h"
 
 /*!
 * \brief Builds a DCM from the given calibrated sensor values
-* \param[out] ddcm The DCM matrix to write to
-* \param[in] ax The x-axis accelerometer value.
-* \param[in] ay The y-axis accelerometer value.
-* \param[in] az The z-axis accelerometer value.
-* \param[in] gx The x-axis gyroscope value.
-* \param[in] gy The y-axis gyroscope value.
-* \param[in] gz The z-axis gyroscope value.
+* \param[out] dcm The DCM matrix to write to
+* \param[in] a The accelerometer vector.
+* \param[in] m The magnetometer vector
 */
-void sensor_dcm(mf16 *const ddcm,
-    register const fix16_t *const ax, register const fix16_t *const ay, register const fix16_t *const az,
-    register const fix16_t *const mx, register const fix16_t *const my, register const fix16_t *const mz)
+void sensor_dcm(mf16 *const dcm,
+    register const v3d *RESTRICT const a, register const v3d *RESTRICT const m)
 {
+    // TODO: check pointer to array dereferencing
 
+#if 0
+
+    // calculate the vector norms for normalization
+    register const fix16_t norm_a = fa16_norm(&(*a)[0], 1, 3);
+    register const fix16_t norm_m = fa16_norm(&(*m)[0], 1, 3);
+
+    // invert norm to speed up following calculations
+    register const fix16_t inv_norm_a = fix16_div(F16(1), norm_a);
+    register const fix16_t inv_norm_m = fix16_div(F16(1), norm_m);
+
+    // normalize vectors
+    const fix16_t an[3] = {
+        fix16_mul((*a)[0], inv_norm_a),
+        fix16_mul((*a)[1], inv_norm_a),
+        fix16_mul((*a)[2], inv_norm_a)
+    };
+
+    const fix16_t mn[3] = {
+        fix16_mul((*m)[0], inv_norm_m),
+        fix16_mul((*m)[1], inv_norm_m),
+        fix16_mul((*m)[2], inv_norm_m)
+    };
+
+#else
+
+    v3d an, mn;
+    v3d_normalize(&an, a);
+    v3d_normalize(&mn, m);
+
+#endif
+
+    // define coordinate system
+    v3d X, Y, Z;
+
+    // after normalization, a (positive up) is the Z axis
+    Z = an;
+
+    // after normalization, m lies along the X axis
+    // note that X and Z are most probably not orthogonal due to the
+    // fact that m follows the magnetic field.
+    // we will correct for that later
+    X = mn;
+
+    // now we calculate Y by crossing X and Z and renormalize
+    v3d_cross(&Y, &X, &Z);
+    v3d_normalize(&Y, &Y);
+
+    // finally we recreate X from Z and Y
+    v3d_cross(&X, &Z, &Y);
+    v3d_normalize(&X, &X);
 
     /*
         The direction cosine matrix is defined as the matrix of dot products
@@ -32,17 +79,30 @@ void sensor_dcm(mf16 *const ddcm,
         essentially only select components.
     */
 
-    fix16_t x[3], y[3], z[3];
+    dcm->data[0][0] = X.x;
+    dcm->data[0][1] = Y.x;
+    dcm->data[0][2] = Z.x;
 
-    ddcm->data[0][0] = x[0];
-    ddcm->data[0][1] = y[0];
-    ddcm->data[0][2] = z[0];
+    dcm->data[1][0] = X.y;
+    dcm->data[1][1] = Y.y;
+    dcm->data[1][2] = Z.y;
 
-    ddcm->data[1][0] = x[1];
-    ddcm->data[1][1] = y[1];
-    ddcm->data[1][2] = z[1];
+    dcm->data[2][0] = X.z;
+    dcm->data[2][1] = Y.z;
+    dcm->data[2][2] = Z.z;
+}
 
-    ddcm->data[2][0] = x[2];
-    ddcm->data[2][1] = y[2];
-    ddcm->data[2][2] = z[2];
+/*!
+* \brief Gets roll, pitch, yaw from DCM.
+* \param[out] roll The roll angle in radians.
+* \param[out] pitch The pitch(elevation) angle in radians.
+* \param[out] yaw The yaw(heading, azimuth) angle in radians.
+*/
+void sensor_dcm2rpy(mf16 *const dcm, fix16_t *RESTRICT const roll, fix16_t *RESTRICT const pitch, fix16_t *RESTRICT const yaw)
+{
+    // extract angles
+    // see: William Premerlani, "Computing Euler Angles from Direction Cosines"
+    *pitch = -fix16_asin(dcm->data[0][2]);
+    *roll = fix16_atan2(dcm->data[1][2], dcm->data[2][2]);
+    *yaw = fix16_atan2(dcm->data[0][1], dcm->data[0][0]);
 }
