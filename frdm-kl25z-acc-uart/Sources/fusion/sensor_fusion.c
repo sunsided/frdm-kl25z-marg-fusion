f616ee69020766a46be593d0be0c9a1db3a157b8
Markus Mayer <development@widemeadows.de>
2014-01-27 02:35:30 +0100

    alternative quaternion function did not work well

--- frdm-kl25z-acc-uart/Sources/fusion/sensor_fusion.c
+++ frdm-kl25z-acc-uart/Sources/fusion/sensor_fusion.c
@@ -17,13 +17,13 @@
 /* Measurement covariance definitions                                   */
 /************************************************************************/
 
-static const fix16_t initial_r_axis = F16(0.02);
-static const fix16_t initial_r_gyro = F16(0.001);
+static const fix16_t initial_r_axis = F16(0.005);
+static const fix16_t initial_r_gyro = F16(0.01);
 
 static const fix16_t q_axis = F16(0);
-static const fix16_t q_gyro = F16(.1);
+static const fix16_t q_gyro = F16(4.36);
 
-static const fix16_t alpha1 = F16(100);
+static const fix16_t alpha1 = F16(10);
 static const fix16_t alpha2 = F16(0.4);
 
 /************************************************************************/
@@ -529,6 +529,22 @@ STATIC_INLINE int32_t fix16_sign_ex(const fix16_t value)
     return (value > 0) ? 1 : (value < 0) ? -1 : 0;
 }
 
+/*!*
+* \brief Calculates the norm of a three component vector
+*/
+HOT CONST
+STATIC_INLINE fix16_t norm3(register const fix16_t a, register const fix16_t b, register const fix16_t c) {
+    return fix16_sqrt(fix16_add(fix16_sq(a), fix16_add(fix16_sq(b), fix16_sq(c))));
+}
+
+/*!*
+* \brief Calculates the norm of a two component vector
+*/
+HOT CONST
+STATIC_INLINE fix16_t norm2(register const fix16_t a, register const fix16_t b) {
+    return fix16_sqrt(fix16_add(fix16_sq(a), fix16_sq(b)));
+}
+
 /*!
 * \brief Fetches the values without any modification
 * \param[out] roll The roll angle in radians.
@@ -561,10 +577,10 @@ STATIC_INLINE void calculate_roll_pitch(register fix16_t *RESTRICT const roll, r
     const mf16 *const x = kalman_get_state_vector_uc(&kf_attitude);
 
     // fetch axes
-    const fix16_t c31 =  x->data[0][0];
-    const fix16_t c32 =  x->data[1][0];
-    const fix16_t c33 =  x->data[2][0];
-
+    fix16_t c31 =  x->data[0][0];
+    fix16_t c32 =  x->data[1][0];
+    fix16_t c33 =  x->data[2][0];
+    
     // calculate pitch
     const fix16_t c2sq = fix16_sq(c32);
     const fix16_t c3sq = fix16_sq(c33);
@@ -651,8 +667,8 @@ STATIC_INLINE const fix16_t zero_or_value(register const fix16_t value)
 * \param[out] quat The orientation quaternion
 */
 HOT NONNULL LEAF
-void fusion_fetch_quaternion(register qf16 *RESTRICT const quat)
-{   
+static void fetch_quaternion_opt1(register qf16 *RESTRICT const quat)
+{
     const register mf16 *const x2 = kalman_get_state_vector_uc(&kf_orientation);
     const register mf16 *const x3 = kalman_get_state_vector_uc(&kf_attitude);
 
@@ -679,7 +695,7 @@ void fusion_fetch_quaternion(register qf16 *RESTRICT const quat)
     register fix16_t m02 = fix16_sub(fix16_mul(m10, m21), fix16_mul(m11, m20));
 
     // normalize C1 
-    const register fix16_t norm = fix16_sqrt(fix16_add(fix16_sq(m00), fix16_add(fix16_sq(m01), fix16_sq(m02))));
+    const register fix16_t norm = norm3(m00, m01, m02);
     m00 = fix16_div(m00, norm);
     m01 = fix16_div(m01, norm);
     m02 = fix16_div(m02, norm);
@@ -719,7 +735,7 @@ void fusion_fetch_quaternion(register qf16 *RESTRICT const quat)
     //               =  1 - (m00 + m11 - m22)
     //               =  1 - (m00 + (m11 - m22))
     fix16_t qz = fix16_mul(F16(0.5), fix16_sqrt(zero_or_value(fix16_sub(F16(1), fix16_add(m00, fix16_sub(m11, m22))))));
-    
+
 #if 1
 
     // qx = copysign(qx, m21 - m12);
@@ -743,7 +759,7 @@ void fusion_fetch_quaternion(register qf16 *RESTRICT const quat)
     qz *= fix16_sign_ex(fix16_sub(m01, m10));
 
 #endif
-    
+
     // compose quaternion
     quat->a = qw;
     quat->b = qx;
@@ -752,6 +768,140 @@ void fusion_fetch_quaternion(register qf16 *RESTRICT const quat)
 
     // normalizify
     qf16_normalize(quat, quat);
+}
+
+/*!
+* \brief Fetches the orientation quaternion.
+* \param[out] quat The orientation quaternion
+*/
+HOT NONNULL LEAF
+static void fetch_quaternion_opt2(register qf16 *RESTRICT const quat)
+{   
+    const register mf16 *const x2 = kalman_get_state_vector_uc(&kf_orientation);
+    const register mf16 *const x3 = kalman_get_state_vector_uc(&kf_attitude);
+
+    // m00 = R(1, 1);    m01 = R(1, 2);    m02 = R(1, 3);
+    // m10 = R(2, 1);    m11 = R(2, 2);    m12 = R(2, 3);
+    // m20 = R(3, 1);    m21 = R(3, 2);    m22 = R(3, 3);
+
+    const register fix16_t m10 = x2->data[0][0];
+    const register fix16_t m11 = x2->data[1][0];
+    const register fix16_t m12 = x2->data[2][0];
+
+    const register fix16_t m20 = x3->data[0][0];
+    const register fix16_t m21 = x3->data[1][0];
+    const register fix16_t m22 = x3->data[2][0];
+
+    // calculate cross product for C1
+    // m0 = cross([m10 m11 m12], [m20 m21 m22])
+    // -->
+    //      m00 = m11*m22 - m12*m21
+    //      m01 = m12*m20 - m10*m22
+    //      m02 = m10*m21 - m11*m20
+    register fix16_t m00 = fix16_sub(fix16_mul(m11, m22), fix16_mul(m12, m21));
+    register fix16_t m01 = fix16_sub(fix16_mul(m12, m20), fix16_mul(m10, m22));
+    register fix16_t m02 = fix16_sub(fix16_mul(m10, m21), fix16_mul(m11, m20));
+
+    // normalize C1 
+    const register fix16_t norm = norm3(m00, m01, m02);
+    m00 = fix16_div(m00, norm);
+    m01 = fix16_div(m01, norm);
+    m02 = fix16_div(m02, norm);
+
+    // "Angel" code
+    // http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
+
+    fix16_t qw, qx, qy, qz;
+
+    // check the matrice's trace
+    const register fix16_t trace = fix16_add(m00, fix16_add(m11, m22));
+    if (trace > 0)
+    {
+        /*
+        s = 0.5 / sqrt(trace + 1.0);
+        qw = 0.25 / s;
+        qx = ( R(3,2) - R(2,3) ) * s;
+        qy = ( R(1,3) - R(3,1) ) * s;
+        qz = ( R(2,1) - R(1,2) ) * s;
+        */
+
+        const fix16_t s = fix16_div(F16(0.5), fix16_sqrt(fix16_add(F16(1.0), trace)));
+
+        qw = fix16_div(F16(0.25), s);
+        qx = fix16_mul(fix16_sub(m21, m12), s);
+        qy = fix16_mul(fix16_sub(m02, m20), s);
+        qz = fix16_mul(fix16_sub(m10, m01), s);
+    }
+    else
+    {
+        if (m00 > m11 && m00 > m22)
+        {
+            /*
+            s = 2.0 * sqrt( 1.0 + R(1,1) - R(2,2) - R(3,3));
+            qw = (R(3,2) - R(2,3) ) / s;
+            qx = 0.25 * s;
+            qy = (R(1,2) + R(2,1) ) / s;
+            qz = (R(1,3) + R(3,1) ) / s;
+            */
+            const fix16_t s = fix16_mul(F16(2), fix16_sqrt(fix16_add(F16(1), fix16_sub(m00, fix16_add(m11, m22)))));
+
+            qw = fix16_div(fix16_sub(m21, m12), s);
+            qx = fix16_mul(F16(0.25), s);
+            qy = fix16_div(fix16_add(m01, m10), s);
+            qz = fix16_div(fix16_add(m02, m20), s);
+        }
+        else if (m11 > m22)
+        {
+            /*
+            s = 2.0 * sqrt( 1.0 + R(2,2) - R(1,1) - R(3,3));
+            qw = (R(1,3) - R(3,1) ) / s;
+            qx = (R(1,2) + R(2,1) ) / s;
+            qy = 0.25 * s;
+            qz = (R(2,3) + R(3,2) ) / s;
+            */
+            const fix16_t s = fix16_mul(F16(2), fix16_sqrt(fix16_add(F16(1), fix16_sub(m11, fix16_add(m00, m22)))));
+
+            qw = fix16_div(fix16_sub(m02, m20), s);
+            qx = fix16_div(fix16_add(m01, m10), s);
+            qy = fix16_mul(F16(0.25), s);
+            qz = fix16_div(fix16_add(m12, m21), s);
+        }
+        else
+        {
+            /*
+            s = 2.0 * sqrt( 1.0 + R(3,3) - R(1,1) - R(2,2) );
+            qw = (R(2,1) - R(1,2) ) / s;
+            qx = (R(1,3) + R(3,1) ) / s;
+            qy = (R(2,3) + R(3,2) ) / s;
+            qz = 0.25 * s;
+            */
+            const fix16_t s = fix16_mul(F16(2), fix16_sqrt(fix16_add(F16(1), fix16_sub(m22, fix16_add(m00, m11)))));
+
+            qw = fix16_div(fix16_sub(m10, m01), s);
+            qx = fix16_div(fix16_add(m02, m20), s);
+            qy = fix16_div(fix16_add(m12, m21), s);
+            qz = fix16_mul(F16(0.25), s);
+        }
+    }
+
+    // compose quaternion
+    quat->a = qw;
+    quat->b = qx;
+    quat->c = qy;
+    quat->d = qz;
+
+    // normalizify
+    qf16_normalize(quat, quat);
+}
+
+/*!
+* \brief Fetches the orientation quaternion.
+* \param[out] quat The orientation quaternion
+*/
+HOT NONNULL LEAF
+void fusion_fetch_quaternion(register qf16 *RESTRICT const quat)
+{
+    fetch_quaternion_opt1(quat);
 }
 
 /************************************************************************/
@@ -1093,7 +1243,7 @@ static void fusion_update_orientation(register const fix16_t deltaT)
     fix16_t mx, my, mz;
     fix16_t cos_pitch = magnetometer_project(&mx, &my, &mz);
     
-#if 0
+#if 1
     // check for singularity
     if (cos_pitch < F16(0.17365))
     {
@@ -1148,8 +1298,8 @@ static void fusion_update_orientation(register const fix16_t deltaT)
 */
 void fusion_update(register const fix16_t deltaT)
 {
-#if 1
-#if 1 // force gyro-only
+#if 0
+#if 0 // force gyro-only
     if (m_attitude_bootstrapped && m_orientation_bootstrapped)
     {
         m_have_accelerometer = false;
